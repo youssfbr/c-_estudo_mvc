64cb9ca769ee4556fa2c41726eaa7b495fcbfaf0
Alisson Youssf <youssfbr@gmail.com>
2020-01-13 19:23:52 -0300

    Configurado Swagger

--- Estudo_API/Startup.cs
+++ Estudo_API/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+// using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Models;
 
 namespace Estudo_API
 {
@@ -26,6 +28,7 @@ namespace Estudo_API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Estudo API", Version = "v1" }); });            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +38,9 @@ namespace Estudo_API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Estudo API"); });
 
             app.UseHttpsRedirection();
 
