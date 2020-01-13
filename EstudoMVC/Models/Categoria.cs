35497de238c0f3de40511cb8ff2a0b6176005711
Alisson Youssf <youssfbr@gmail.com>
2020-01-13 07:33:48 -0300

    Criada Classe Categoria

---
+++ EstudoMVC/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstudoMVC.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+    }
+}
